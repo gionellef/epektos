3b79ac3a732da38da1cfadb189695376131dc456
Gionelle F. Tribiana <gionellef@gmail.com>
2017-04-22 12:44:58 +0800

    Changed numbers

--- app/controllers/family_controller.rb
+++ app/controllers/family_controller.rb
@@ -1,8 +1,8 @@
 class FamilyController < ApplicationController
   def show
     @family = [
-      ["Anna Tero", "+631233567"],
-      ["John Tero", "+631434337"]
+      ["Anna Tero", "+639778330802"],
+      ["John Tero", "+639989661626"]
     ]
   end
 end
